8dd3d78a41ea8bb88f39decc3eb802876fa1d490
Yuri Matsui <yurimatsui37@gmail.com>
2014-06-01 21:33:52 +0900

    create user db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140601120719) do
+ActiveRecord::Schema.define(version: 20140601123015) do
 
   create_table "numbers", force: true do |t|
     t.integer  "number"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20140601120719) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "description"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
